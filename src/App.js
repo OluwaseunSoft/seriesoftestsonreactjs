40369c4a28fea0041547ad88ba33731f95e775db
Oluwaseun <oluwaseunazure@yahoo.com>
2020-06-05 07:05:49 +0100

    useState

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React, {Component} from 'react';
 import './App.css';
 import MessageView from './messages/message-view';
 import MessageList from './messages/message-list';
-import ClassDemo from './Hooks/useState';
+import HookDemo from './Hooks/useState';
 
 class App extends Component{
     render(){         
         return(
-            <ClassDemo/>
+            <HookDemo/>
         )
     }
 }
--- src/Hooks/useState.js
+++ src/Hooks/useState.js
@@ -1,39 +1,54 @@
-import React from "react";
+import React, {useState} from "react";
 
-export default class ClassDemo extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            name: "Agatha"
-        };
-        this.handleNameChange = this.handleNameChange.bind(this);
+// export default class ClassDemo extends React.Component{
+//     constructor(props){
+//         super(props);
+//         this.state = {
+//             name: "Agatha"
+//         };
+//         this.handleNameChange = this.handleNameChange.bind(this);
+//     }
+
+//     handleNameChange(e){
+//         this.setState({
+//             name: e.target.value
+//         });
+//     }
+
+//     render(){
+//         return(
+//             <section>
+                
+//         <p>Hello {this.state.name}</p>
+//             </section>
+//         );
+//     }
+// }
+
+//export default ClassDemo;
+
+export default function HookDemo(props){
+    const [name, setName] = useState("Agata");
+
+    function handleNameChange(e){
+        setName(e.target.value);
     }
 
-    handleNameChange(e){
-        this.setState({
-            name: e.target.value
-        });
-    }
-
-    render(){
-        return(
-            <section>
-                <form autoComplete="off">
+    return(
+        <section>
+           <form autoComplete="off">
                     <section>
                         <label htmlFor="name">Name</label>
                         <input
                             type="text"
                             name="name"
                             id="name"
-                            value={this.state.name}
-                            onChange={this.handleNameChange}
+                            value={name}
+                            onChange={handleNameChange}
                         />
                     </section>
                 </form>
-        <p>Hello {this.state.name}</p>
-            </section>
-        );
-    }
+                <p>Hello {name}</p>
+        </section>
+    );
 }
-
-//export default ClassDemo;
